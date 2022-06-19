8d23183e4a9a7534a3d7456cbd595ecdc9793c6a
ginlink <ginlink1998@gmail.com>
2022-06-19 09:18:30 +0800

    chore: change client url

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { ApolloProvider } from 'react-apollo'
 import { client } from './apollo/client'
